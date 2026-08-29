508ece822f79d68065c0103b677cddb4df09c262
Iliaskokkal <kokkiliasgrandera@gmail.com>
2026-08-29 17:10:20 +0300

    create new project

---
+++ src/main/java/gr/healayra/healayrabackend/HealayraBackendApplication.java
@@ -0,0 +1,13 @@
+package gr.healayra.healayrabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealayraBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealayraBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gr/healayra/healayrabackend/HealayraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.healayra.healayrabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealayraBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
