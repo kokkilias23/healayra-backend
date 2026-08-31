44734601ccb1d347e30b9694feeb81f231c027cc
Iliaskokkal <kokkiliasgrandera@gmail.com>
2026-09-01 00:50:16 +0300

    feat: add JWT authentication and security configurations

---
+++ src/main/java/gr/healayra/backend/security/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package gr.healayra.backend.security;
+
+import gr.healayra.backend.model.User;
+import gr.healayra.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException(
+                                "User not found with email: " + email
+                        )
+                );
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
